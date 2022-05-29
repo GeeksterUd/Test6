e7698cc0d988b7569825c434c2cf0e67557ef53b
umesh singh <uditsingh007cr7@gmail.com>
2022-05-29 16:04:58 +0530

    completed the test

---
+++ week.js
@@ -0,0 +1,27 @@
+let n=1;
+switch(n){
+    case 0:
+    console.log('It is Sunday')
+    break;
+    case 1:
+    console.log('It is Monday')
+    break;
+    case 2:
+    console.log('It is Tuesday')
+    break;
+    case 3:
+    console.log('It is Wednesday')
+    break;
+    case 4:
+    console.log('It is Thursday')
+    break;
+    case 5:
+    console.log('It is Friday')
+    break;
+    case 6:
+    console.log('It is Saturday')
+    break;
+    default:
+    console.log('invalid')
+    break;
+}
